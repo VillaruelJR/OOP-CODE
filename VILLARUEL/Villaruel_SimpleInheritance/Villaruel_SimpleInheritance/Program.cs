f0aed1be9b8bc77f2a5c3ca288cf507de2d26f64
VillaruelJR <ingoroel@gmail.com>
2025-09-01 10:56:33 +0800

    Add files via upload

---
+++ VILLARUEL/Villaruel_SimpleInheritance/Villaruel_SimpleInheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villaruel_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //JOHN ROBERT C. VILLARUEL
+            //BSIT-301
+            //TITLE: Simple Inheritance
+            
+            RegularStudent student = new RegularStudent();
+            student.Name = "JOHN ROBERT VILLARUEL";
+            student.Program = "BSIT";
+            student.Section = "301";
+           
+ 
+
+            IrregularStudent student1 = new IrregularStudent();
+            student1.Name = "JOHN ROBERT VILLARUEL";
+            student1.Program = "BSIT";
+            student1.UnitEnrolled = "20";
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VILLARUEL/Villaruel_SimpleInheritance/Villaruel_SimpleInheritance/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Villaruel_SimpleInheritance;
+
+namespace Villaruel_SimpleInheritance
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        public string Program { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+        }
+    }
+
+    class  RegularStudent : Student
+    {
+        public string Section { get; set; }     
+        
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section of Student:" + Section);
+        }
+    }
+}
+
+    class IrregularStudent : Student
+{
+    public string UnitEnrolled { get; set; }
+
+    public void EnrolledSemUnits()
+    {
+        Console.WriteLine("Student Units Enrolled:" + UnitEnrolled);
+    }
+}
